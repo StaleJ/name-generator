2913e6ba10ecdd0ba50c89fa2419e02f9efe51b8
Ståle Jacobsen <staale.jacobsen@horde.no>
2025-10-20 10:52:46 +0200

    lol

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Bogus;
+
+var faker = new Faker("nb_NO");
+var output = $"""
+              Fornavn: {faker.Person.FirstName}
+              Etternavn: {faker.Person.LastName}
+              Telefon: {faker.Person.Phone}
+              """;
+Console.WriteLine(output);
